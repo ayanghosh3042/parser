225a468378237374b9d84a36afd0263d0ea27509
Prerna <prerna.mishra@mayadataprivacy.eu>
2026-04-08 18:23:53 +0530

    Sidebar: Profile added

--- Frontend/Design/InputField.tsx
+++ Frontend/Design/InputField.tsx
@@ -1,14 +1,15 @@
 import { ChangeEvent } from "react";
 
 interface Props {
-  label: string;
-  name: string;
+  label?: string;
+  name?: string;
   type?: string;
+  value?: string;
   placeholder?: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const InputField = ({ label, name, type = "text", placeholder, onChange }: Props) => {
+const InputField = ({ label, name, type = "text", value, placeholder, onChange }: Props) => {
   return (
     <div className="space-y-1">
       <label className="text-slate-700 text-sm font-medium">
@@ -17,6 +18,7 @@ const InputField = ({ label, name, type = "text", placeholder, onChange }: Props
       <input
         type={type}
         name={name}
+        value={value}
         placeholder={placeholder}
         onChange={onChange}
         className="w-full h-10 rounded-md px-3 bg-white text-gray-700 border border-gray-300 focus:outline-none placeholder:text-base"
